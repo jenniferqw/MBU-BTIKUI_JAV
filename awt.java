d5a98e2a65b20b04f86cccf989555de239c9531a
DarshanKansara504 <109453833+DarshanKansara504@users.noreply.github.com>
2022-10-01 01:17:29 +0530

    Update awt.java

--- awt.java
+++ awt.java
@@ -21,7 +21,22 @@ class MenuExample
          f.setMenuBar(mb);  
          f.setSize(400,400);  
          f.setLayout(null);  
-         f.setVisible(true);  
+         f.setVisible(true);
+          int num = 29;
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
 }  
 public static void main(String args[])  
 {  
