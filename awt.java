810f8764654ea08a959c39d81bdd8d3348664965
nehabsaxena <111478678+nehabsaxena@users.noreply.github.com>
2022-10-01 13:30:56 +0530

    awt java tech stack

--- awt.java
+++ awt.java
@@ -49,6 +49,14 @@ System.out.println(is
 }  
 public static void main(String args[])  
 {  
-new MenuExample();  
+new MenuExample();  ible(true);
+          int num = 29;
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
 }  
 }  
